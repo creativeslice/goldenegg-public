8538e351a2cffffe938c25c122cd624801bcef8b
Tim Bowen <tim@creativeslice.com>
2014-04-28 11:04:30 -0700

    Adding 4 sample grids to use as examples

--- page-styles.php
+++ page-styles.php
@@ -16,78 +16,7 @@
 
 				<section class="entry-content">
 					
-					<div class="cf">
-						<div class="goldlarge">
-							<p><strong>goldlarge</strong><br />Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
-						</div>
-						<div class="goldsmall">
-							<p><strong>goldsmall</strong><br /> Right sidebar</p>
-						</div>
-					</div>
-					
-					<hr>
-					
-					<div class="cf">
-						<div class="l-g1 m-g1 s-g1 first-col">
-							<p><strong>l-g1 m-g1 s-g1 first-col</strong><br /> Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
-						</div>
-						<div class="l-g2 m-g2 s-g2">
-							<p><strong>l-g2 m-g2 s-g2</strong><br /> Text here</p>
-						</div>
-						<div class="l-g3 m-g3 s-g1">
-							<p><strong>l-g3 m-g3 s-g1</strong><br /> Text here too</p>
-						</div>
-						<div class="l-g4 m-all s-g2 last-col">
-							<p><strong>l-g4 m-all s-g2 last-col</strong><br /> Far right column</p>
-						</div>
-					</div>
-					
-					<hr>
-					
-					<div class="cf">
-						<div class="l-g1-g2 m-g1-g2 s-g1">
-							<p><strong>l-g1-g2 m-g1-g2 s-g1</strong><br />Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
-						</div>
-						<div class="l-g3-g4 m-g3 s-g2 last-col">
-							<p><strong>l-g3-g4 m-g3 s-g2 last-col</strong><br /> Right sidebar</p>
-						</div>
-					</div>
-					
-					<hr>
-					
-					<div class="cf">
-						<div class="l-g1-g3 m-g1-g2 s-all">
-							<p><strong>l-g1-g3 m-g1-g2 s-all</strong><br />Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
-						</div>
-						<div class="l-g4 m-g3 s-all last-col">
-							<p><strong>l-g4 m-g3 s-all last-col</strong><br /> Right sidebar</p>
-						</div>
-					</div>
-					
-					<hr>
-					
-					<div class="cf">
-						<div class="l-g1 m-g1 s-g1 first-col">
-							<p><strong>l-g1 m-g1 s-g1 first-col</strong><br /> Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
-						</div>
-						<div class="l-g3-g4 m-g3 s-all last-col">
-							<p><strong>l-g3-g4 m-g3 s-all last-col</strong><br /> Far right column</p>
-						</div>
-					</div>
-					
-					<hr>
-					
 					<?php the_content(); ?>
-					
-					<hr>
-					
-					<h1>Simple H1 <a href="#">Headline</a> with additional <strong>bold text</strong> to make it wrap so we can test <em>italic text</em> and line height.</h1>
-					<h2>Simple H2 <a href="#">Headline</a> with additional <strong>bold text</strong> to make it wrap so we can test <em>italic text</em> and line height.</h2>
-					<h3>Simple H3 <a href="#">Headline</a> with additional <strong>bold text</strong> to make it wrap so we can test <em>italic text</em> and line height.</h3>
-					<h4>Simple H4 <a href="#">Headline</a> with additional <strong>bold text</strong> to make it wrap so we can test <em>italic text</em> and line height.</h4>
-					<h5>Simple H5 <a href="#">Headline</a> with additional <strong>bold text</strong> to make it wrap so we can test <em>italic text</em> and line height.</h5>
-					
-					<hr>
 					
 					<h1>H1 Headline</h1>
 					<p>Paragraph text following an H1 <a href="#">Headline</a> with additional <strong>bold text</strong> to make it wrap so we can test <em>italic text</em> and line height.</p>
@@ -136,11 +65,117 @@
 					
 					<p>Additional paragraph text with <strong><em>bold italic text</em></strong> to make it wrap so we can test <em>italic text</em> and line height. More paragraph text here with additional text to make it wrap so we can test line height. More paragraph text here with additional text to make it wrap so we can test line height. <a class="button" href="#">Inline button</a> More paragraph text here with additional text to make it wrap so we can test line height. More paragraph text here with additional text to make it wrap so we can test line height. More paragraph text here with additional text to make it wrap so we can test line height. More paragraph text here with additional text to make it wrap so we can test line height.</p>
 					
+					<hr>
+					
+					<div class="cf">
+						<h2>Simple Golden Ratio</h2>
+						<div class="goldlarge">
+							<p><strong>goldlarge</strong><br />Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
+						</div>
+						<div class="goldsmall">
+							<p><strong>goldsmall</strong><br /> Right sidebar</p>
+						</div>
+						
+						<div class="goldlarge last-col">
+							<p><strong>goldlarge last-col</strong><br /> Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
+						</div>
+						<div class="goldsmall first-col">
+							<p><strong>goldsmall first-col</strong><br />Right sidebar</p>
+						</div>
+
+					</div>
+					
+					<hr>
+					
+					<div class="cf">
+						<h2>Fonmon Grid</h2>
+						<div class="l-g1 m-g1 s-all">
+							<p><strong>l-g1 m-g1 s-all</strong><br /> Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
+						</div>
+						<div class="l-g2 m-g2 s-g1">
+							<p><strong>l-g2 m-g2 s-g1</strong><br /> Text here</p>
+						</div>
+						<div class="l-g2-g3 m-g1 m-last s-g2 s-last">
+							<p><strong>l-g2-g3 m-g1 m-last s-g2 s-last</strong><br /> Text here too</p>
+						</div>
+						<div class="l-g1 m-all s-all l-last">
+							<p><strong>l-g1 m-all s-all l-last</strong><br /> Far right column</p>
+						</div>
+					</div>
+					
+					<hr>
+					
+					<div class="cf">
+						<h2>Gerstner Grid</h2>
+						<div class="l-g2 m-g1 s-all first-col">
+							<p><strong>l-g2 m-g1 s-all first-col</strong><br /> Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
+						</div>
+						<div class="l-g1 m-g1 s-g1">
+							<p><strong>l-g1 m-g1 s-g1</strong><br /> Text here</p>
+						</div>
+						<div class="l-g1 m-g1 s-g1 s-last">
+							<p><strong>l-g1 m-g1 s-g1 s-last</strong><br /> Text here too</p>
+						</div>
+						<div class="l-g2 m-g1 s-all l-last m-last">
+							<p><strong>l-g2 m-g1 s-all l-last m-last</strong><br /> Far right column</p>
+						</div>
+					</div>
+					
+					<hr>
+					
+					<div class="cf">
+						<h2>Golden Ratio</h2>
+						<div class="l-g1 m-g1 s-g1 first-col">
+							<p><strong>l-g1 m-g1 s-g1 first-col</strong><br /> Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
+						</div>
+						<div class="l-g2 m-g2 s-g2">
+							<p><strong>l-g2 m-g2 s-g2</strong><br /> Text here</p>
+						</div>
+						<div class="l-g3 m-g3 s-g1">
+							<p><strong>l-g3 m-g3 s-g1</strong><br /> Text here too</p>
+						</div>
+						<div class="l-g4 m-all s-g2 last-col">
+							<p><strong>l-g4 m-all s-g2 last-col</strong><br /> Far right column</p>
+						</div>
+					</div>
+					
+					<hr>
+					
+					<div class="cf">
+						<div class="l-g1-g2 m-g1-g2 s-g1">
+							<p><strong>l-g1-g2 m-g1-g2 s-g1</strong><br />Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
+						</div>
+						<div class="l-g3-g4 m-g3 s-g2 last-col">
+							<p><strong>l-g3-g4 m-g3 s-g2 last-col</strong><br /> Right sidebar</p>
+						</div>
+					</div>
+					
+					<hr>
+					
+					<div class="cf">
+						<div class="l-g1-g3 m-g1-g2 s-all">
+							<p><strong>l-g1-g3 m-g1-g2 s-all</strong><br />Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
+						</div>
+						<div class="l-g4 m-g3 s-all last-col">
+							<p><strong>l-g4 m-g3 s-all last-col</strong><br /> Right sidebar</p>
+						</div>
+					</div>
+					
+					<hr>
+					
+					<div class="cf">
+						<div class="l-g1 m-g1 s-g1 first-col">
+							<p><strong>l-g1 m-g1 s-g1 first-col</strong><br /> Just some text here to see how it wraps and see how the padding works, or if it works at all...</p>
+						</div>
+						<div class="l-g3-g4 m-g3 s-all last-col">
+							<p><strong>l-g3-g4 m-g3 s-all last-col</strong><br /> Far right column</p>
+						</div>
+					</div>
+					
+					<hr>
+
 				</section>
 
-				<footer class="article-footer cf">
-
-				</footer>
 
 			</article>
 
