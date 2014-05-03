d17a0e1a9ccf2b92631561bf86c7c576eb629cc3
Tim Bowen <tim@creativeslice.com>
2014-05-02 20:09:41 -0700

    includes folder named 'inc'
    Less typing is good

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
-<?php // include function files from includes directory
+<?php // include function files from inc directory
 
-require_once( 'includes/function-admin.php' );
-require_once( 'includes/function-comments.php' );
-require_once( 'includes/function-cleanup.php' );
-require_once( 'includes/function-enqueue.php' );
-require_once( 'includes/function-post-types.php' );
-require_once( 'includes/function-theme-support.php' );
+require_once( 'inc/function-admin.php' );
+require_once( 'inc/function-comments.php' );
+require_once( 'inc/function-cleanup.php' );
+require_once( 'inc/function-enqueue.php' );
+require_once( 'inc/function-post-types.php' );
+require_once( 'inc/function-theme-support.php' );
 
-// require_once( 'includes/src/seo_meta_data.php' );
+// require_once( 'inc/src/seo_meta_data.php' );
 
 ?>
