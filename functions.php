2ee5cf644dbd0caa917328f73c338c836257be59
CS iMac <contact@creativeslice.com>
2014-05-28 13:51:11 -0700

    Removed include for relative-urls.php

--- functions.php
+++ functions.php
@@ -30,7 +30,6 @@ require_once( 'includes/theme-support.php' );
 require_once( 'includes/enqueue.php' );
 require_once( 'includes/custom-post-types.php' );
 require_once( 'includes/roots-rewrites.php' );
-#require_once( 'includes/relative-urls.php' );
 #require_once( 'includes/nice-search.php' );
 
 /**
