aabbf92c282b45976ceacc10d84b42811e6cc2c0
CS iMac <contact@creativeslice.com>
2014-06-09 14:28:50 -0700

    TinyMCE Revisions
    added 'clean up' button back; removed 'toggle' button; added custom 'no-wrap' button;

--- admin/tinymce.php
+++ admin/tinymce.php
@@ -8,6 +8,7 @@
 // actions
 add_action( 'init', 'egg_tinymce' );
 add_action('admin_head', 'egg_add_magic_button');
+add_action('admin_head', 'egg_add_no_wrap');
 
 // filters
 add_filter( 'tiny_mce_before_init', 'egg_mce_show_buttons_2' );
@@ -27,7 +28,8 @@ function egg_tinymce()
  */
 function egg_mce_buttons( $buttons )
 {
-	$remove = array('strikethrough', 'wp_more');
+	$remove = array('strikethrough', 'wp_more', 'wp_adv');
+	//print_r($buttons);
 	return array_diff($buttons, $remove);
 }
 
@@ -39,7 +41,7 @@ function egg_mce_buttons( $buttons )
 function egg_mce_buttons_2( $buttons )
 {
 	// Remove items
-	$remove  = array('styleselect', 'underline', 'forecolor', 'alignjustify', 'pastetext', 'removeformat', 'wp_help');
+	$remove  = array('styleselect', 'underline', 'forecolor', 'alignjustify', 'pastetext', 'wp_help');
 	return array_diff($buttons, $remove);
 }
 
@@ -68,14 +70,30 @@ function egg_mce_show_buttons_2( $in ) {
  * Will grab the parent node if only part of anchor is selected, or the child node if more than the anchor is selected
  */
 function egg_add_magic_button() {
-	add_filter("mce_external_plugins", "egg_add_tinymce_plugin");
+	add_filter("mce_external_plugins", "egg_add_tinymce_button_plugin");
 	add_filter('mce_buttons', 'egg_register_magic_button');
 }
-function egg_add_tinymce_plugin($plugin_array) {
-   	$plugin_array['egg_magic_button'] = get_template_directory_uri().'/assets/js/mce-magic-button.js'; 
+function egg_add_tinymce_button_plugin($plugin_array) {
+   	$plugin_array['egg_magic_button'] = get_template_directory_uri().'/assets/js/tinymce-functions.js'; 
    	return $plugin_array;
 }
 function egg_register_magic_button($buttons) {
    array_push($buttons, "egg_magic_button");
+   return $buttons;
+}
+
+/**
+ * Adds "No-Wrap" - which inserts the selection into a span element with class='nospan'
+ */
+function egg_add_no_wrap() {
+	add_filter("mce_external_plugins", "egg_add_tinymce_no_wrap_plugin");
+	add_filter('mce_buttons', 'egg_register_no_wrap');
+}
+function egg_add_tinymce_no_wrap_plugin($plugin_array) {
+   	$plugin_array['egg_no_wrap'] = get_template_directory_uri().'/assets/js/tinymce-functions.js'; 
+   	return $plugin_array;
+}
+function egg_register_no_wrap($buttons) {
+   array_push($buttons, "egg_no_wrap");
    return $buttons;
 }
