b915e3c1b8b35ea4ecf5d661f2bdfa808b9034c8
Trip Grass <wjgrass@gmail.com>
2014-07-09 14:18:29 -0700

    add egg calendar

--- includes/egg-calendar.php
+++ includes/egg-calendar.php
@@ -6,8 +6,8 @@
 
 class calendar{
 	/** VARIABLES **/
- 	const DEFAULT_START_TYPE = 'natural';								// Options: 'selected' starts on selected time; 'natural' starts at beginning of range;
- 	const DEFAULT_RANGE_UNITS = 'WEEK';									// Options use time constants defined below
+ 	const DEFAULT_START_TYPE = 'selected';								// Options: 'selected' starts on selected time; 'natural' starts at beginning of range;
+ 	const DEFAULT_RANGE_UNITS = 'MONTH';									// Options use time constants defined below
  	const DEFAULT_RANGE = '1';											// Sets range of time shown in range units (i.e., 1 WEEK)
 	const DEFAULT_INCREMENT = 'DAY';									// Time constant less than range
 	
@@ -38,8 +38,10 @@ class calendar{
 
 	/** METHODS **/
 	public function init(){
-		$this->get_date( 'selected' );
-		$this->get_date( 'now' );
+		$selected_stamp =	$this->selected->unixstamp;	
+		$now_stamp = $this->now->unixstamp;	
+		$this->selected = $this->get_date( $selected_stamp );
+		$this->now = $this->get_date( $now_stamp );
 		if( !$this->range_units ){
 			$this->range_units = self::DEFAULT_RANGE_UNITS;			
 		}
@@ -52,14 +54,28 @@ class calendar{
 		if( !$this->range_multiple ){
 			$this->range_multiple = self::DEFAULT_RANGE;
 		}
-		$range_unit_in_seconds = constant( 'calendar::'.$this->range_units );
-		$this->range_in_seconds = $range_unit_in_seconds * $this->range_multiple;
+		if( 'MONTH' == $this->range_units ){
+			$this->range_unit_in_seconds = $this->selected->days_in_month * self::DAY; 
+		}
+		elseif( 'YEAR' == $this->range_units ){
+           if ($this->selected->year % 400 == 0 OR ($this->selected->year % 4 == 0 AND $this->selected->year % 100 != 0)){
+       			$this->range_unit_in_seconds = constant( 'calendar::'.$this->range_units ) + constant( 'calendar::Day');
+            }
+			else{
+	 			$this->range_unit_in_seconds = constant( 'calendar::'.$this->range_units );
+			}
+		}
+		else{
+			$this->range_unit_in_seconds = constant( 'calendar::'.$this->range_units );
+		}
+		$this->range_in_seconds = $this->range_unit_in_seconds * $this->range_multiple;
 		$this->start_of_range = $this->get_start_of_increment( $this->range_units , $this->selected->unixstamp);
-	//	Loop over requested increments and order array in descending order
-		$this->increment_array[] = $this->range_units;
+//print_r($this);
+//		$this->increment_array[] = $this->range_units;
 		if( count( $this->increment_array ) > 0 ){
 			$sorted_increment_array = array();
 			foreach( $this->increment_array as $increment_name ){
+
 				$increment_name = strtoupper( $increment_name );
 				if( 'MONTH' == $increment_name ){
 					$key = $this->selected->days_in_month * self::DAY; 
@@ -70,7 +86,9 @@ class calendar{
 					}
 					$key = constant( 'self::'.$increment_name ); 	// gets increment time in seconds to use as key 	
 				}
-				$sorted_increment_array[$key] = array( 'seconds'=>$key, 'name'=>$increment_name );
+				if( $key < $this->range_unit_in_seconds ){
+					$sorted_increment_array[$key] = array( 'seconds'=>$key, 'name'=>$increment_name );
+				}
 			}
 			arsort($sorted_increment_array);
 			$this->increment_array = $sorted_increment_array ;
@@ -83,14 +101,8 @@ class calendar{
 		if( !$this->start_type || ( $this->start_type != 'natural' && $this->start_type != 'selected' ) ){
 			$this->start_type = self::DEFAULT_START_TYPE;
 		}
-		if( 'selected' == $this->start_type ){										
-			$this->start_time = '1'; 								//round to beginning of lowest increment
-		}
-		else{
-			$this->get_start_of_increments();
-		}
-//print_r($this);
-
+		$this->build_calendar_array();
+		$this->output_calendar();
 	}
 	public function get_start_of_increment( $increment, $selected_time ){
 		if( 'DAY' == $increment ){
@@ -112,104 +124,105 @@ class calendar{
 			$this->increment_array[$seconds]['start_time_of_unit'] = $this->get_start_of_increment( $array['name'] , $selected_time );
 		}		
 	}
-	public function generate(){
-		$increment_array = $this->increment_array;
-		$this->generate_units( $increment_array );
-	}
-	public function generate_units( $increment_array ){
-		$unit_array = array_shift( $increment_array );
-		$output = array();
-		$seconds = $unit_array['seconds'];
-		$name = $unit_array['name'];
-		$unit_count = $counter = floor( $this->range_in_seconds / $seconds);
-		while($counter > 0){
-			$key = $unit_count - $counter;
-			$output[$name][$key] = array(); 
-			$counter--;
-		}	
-		if( count($increment_array )> 0 ){
-			$this->generate_units( $increment_array );
+	public function level_array(){
+		$leval_array = array();
+		$array = $this->increment_array;
+		arsort($array);
+		foreach( $array as $key=>$arr){	
+			$level_array[] = $arr;
 		}
-		//print_r($output);
+		return $level_array;
 	}
-	
-	public function Oldgenerate_unit( ){
-//	print_r($this);
-		foreach( $this->increment_array as $seconds=>$increment_array ){
-			$name = $increment_array['name'];
-			$start_time_of_unit = $counter = $this->increment_array[$seconds]['start_time_of_unit'];
-//return textdate based on unit get_textdate( $seconds );
-			$output[$name][$start_time_of_unit] = array( 'textdate'=>date( ' F j Y', $counter));
-			//echo $counter;
-			
-			$this->increment_array[$seconds]['number_of_units_counter'] = floor( $this->range_in_seconds / $seconds);
-			while( $counter < ($this->start_of_range + $seconds) ){
-echo "seconds".$seconds."<br>";
-//			echo "builder";
-				$output[$name][$start_time_of_unit]['DAY'][$counter] = array( 'textdate'=>date( ' F j Y', $counter) );
-//				echo "Date:".date( ' F j Y', $counter)."<br>";
-				$counter = $counter + self::DAY;
-				
-				$this->increment_array[$seconds]['number_of_units_counter']--;
+	public function get_multiple( $major, $minor ){
+		$major_seconds = constant( 'self::'.$major);
+		$minor_seconds = constant( 'self::'.$minor);
+		if( 'MONTH' == $major ){
+			$major_seconds =  $this->selected->days_in_month * self::DAY; 			
+		}
+		$multiple =  floor( $major_seconds / $minor_seconds);
+		return $multiple;
+	}
+	public function build_calendar_array( ){
+		$level_array = $this->level_array();
+		$x = count($level_array) - 1;
+		$temp = array();
+		for ($i = $x; $i>=0; $i--){
+			$minor = $level_array[$i]['name'];
+
+			if( $i >= 1){
+				$major = $level_array[$i-1]['name'];
 			}
-			
+			else{
+				$major = $this->range_units;	
+			}
+			$multiple = $this->get_multiple( $major, $minor );
+			$start_time = $this->start_of_range;
+			$level_increment = $level_array[$i]['seconds'];
+			$counter = 0;
+			$unittemp = array();
+			while ( $counter < $multiple  ){
+				$id = $start_time + ($counter * $level_increment); 
+				$unittemp[$id] = array( $temp );
+				$counter++;
+			}
+			$array = array( $level_array[$i]['name'] => $unittemp );
+			$temp = $array;
 		}
-		print_r($output);
+		$this->calendar_array = $temp;
+		return $temp;
 	}
-	public function get_date( $option ){
-		$sel = $this->$option;
-		$stamp = $sel->unixstamp;
+	public function get_date( $stamp ){
+		$sel = new stdClass;
+		$sel->unixstamp = $stamp;
 		$sel->start_of_day = strtotime( 'today', $stamp );
 		$sel->day = date( 'l', $stamp );
 		$sel->day_of_week = date( 'N', $stamp ); 
 		$sel->day_of_month = date( 'd', $stamp );
 		$sel->month = date( 'm', $stamp );
-		$sel->month_name = $month_name[ date( 'm', $stamp ) ];
-		$this->get_total_days( $option ) ;
 		$sel->year = date( 'Y', $stamp ); 
-		$sel->secs_from_midnight = $stamp - $sel->start_of_day;
+		$sel->days_in_month = $this->get_total_days( $sel ) ;
 		$sel->time = date( 'G:i e', $stamp );
+		return $sel;
 	}
-	
-    // Total days in a given month - checks for leap year
-    public function get_total_days( $option ){
-    	$sel = $this->$option;
-        $days_in_month = array('01' => 31, '02' => 28,	'03' => 31, '04' => 30,	'05' => 31, '06' => 30,	'07' => 31, '08' => 31, '09' => 30, '10' => 31, '11' => 30, '12' => 31 );
-        if ($sel->month == 02){
-            if ($sel->year % 400 == 0 OR ($sel->year % 4 == 0 AND $sel->year % 100 != 0)){
-                $sel->days_in_month = 29;
+    public function get_total_days( $time_obj ){
+		$days_in_month = array('01' => 31, '02' => 28,	'03' => 31, '04' => 30,	'05' => 31, '06' => 30,	'07' => 31, '08' => 31, '09' => 30, '10' => 31, '11' => 30, '12' => 31 );
+        if ($time_obj->month == 02){
+            if ($time_obj->year % 400 == 0 OR ($time_obj->year % 4 == 0 AND $time_obj->year % 100 != 0)){
+                return 29;
             }
         }
-		$sel->days_in_month = $days_in_month[$sel->month];
+		return $days_in_month[$time_obj->month];
     }
-	public function build_unit(){
-		$unit = $this->unit;
-		call_user_func('calendar::build_'.$unit);
-    }
-    public function build_day(){
-    
-	    echo "day";
+	/** OUTPUT **/
+	function output_calendar( ){
+		$out .= "<div class='cal-".$cal->range_units."'>\n";
+		foreach($this->calendar_array as $unit_name=>$unit_array){
+			$counter = 0;
+			$out .=" <div class='cal-WEEK'> \n";
+			$first_day = date ( 'w' , $this->start_of_range);
+			$empty_days = 0;
+			while($empty_days < $first_day){
+				$out .= " <div class='cal-EMPTYDAY'></div>\n";				
+				$empty_days++;
+			}
+			foreach($unit_array as $timestamp=>$timestamp_array){
+				$counter++;
+				if( date ('w', $timestamp) < 1 ){
+					$out .="<div class='cal-WEEK'> \n";
+				}
+				$out .=" <div class='cal-".$unit_name."' data-timestamp='".$timestamp."'>".date ('D M j Y ', $timestamp);
+				if( $events = get_event( $timestamp) ){
+					foreach( $events as $event_time=>$event_arr){
+						$out .= "\n<div class='cal-EVENT'>".$event_arr->post_title."</div>";
+					}
+				}
+				$out .="</div>\n";
+				if(  date ( 'w' , $timestamp ) == 6 || $counter == count($unit_array)){
+					$out .= "</div><!--/cal-WEEK-->\n";
+				}
+			}
+		}
+		$out .= "</MONTH>\n";
+		echo $out;
 	}
-    public function build_week(){
-	    echo "week";
-    }
-    public function build_month(){
-	    
-    }
-    public function prev_url()
-    {
-        $date = $this->_adjust_date($this->current_month - 1, $this->current_year);
-        return str_replace(array('%m', '%y'), array($date['month'], $date['year']), $this->url);
-    }
-    
-    public function next_url()
-    {
-        $date = $this->_adjust_date($this->current_month + 1, $this->current_year);
-        return str_replace(array('%m', '%y'), array($date['month'], $date['year']), $this->url);
-    }
-    
-    public function weeks()
-    {
-        return $this->weeks;
-    } 
 }
