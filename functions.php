83a00983c2c36db20185fcca32c09a78f9d0e904
CS iMac <contact@creativeslice.com>
2014-07-10 18:43:03 -0700

    One Time Code
    Proposal for running one time code. Save your self-executing one-time-code php file (for database modifications etc) in the *theme*includes/onetimecode folder. Uncomment the 'requires(one-time-code.php)' in the functions.php. Load a page. The code runs and the file is deleted.

--- functions.php
+++ functions.php
@@ -39,3 +39,4 @@ require_once( 'includes/related-posts.php' );
 #require_once( 'includes/xml-sitemap.php' );			// XML Sitemap
 #require_once( 'includes/cleanup-plugins.php' );		// Cleanup commonly used plugins
 #require_once( 'includes/assets-rewrites.php' );		// Rewrite theme assets to /assets and plugins to /plugins. Does not work on nginx servers.
+#require_once( 'includes/one-time-code.php' );
