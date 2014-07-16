31b191d553369118568aac39ab13026de4727ff8
CS iMac <contact@creativeslice.com>
2014-07-16 15:48:10 -0700

    Test and clean up on javascript plugins

--- assets/js/modules/theme-example.js
+++ assets/js/modules/theme-example.js
@@ -30,10 +30,53 @@ var timeToWaitForLast = 100;
 
 
 /*
+ * Here's an example so you can see how we're using the above function
+ *
+ * This is commented out so it won't work, but you can copy it and
+ * remove the comments.
+ *
+ *
+ *
+ * If we want to only do it on a certain page, we can setup checks so we do it
+ * as efficient as possible.
+ *
+ * if( typeof is_home === "undefined" ) var is_home = $('body').hasClass('home');
+ *
+ * This once checks to see if you're on the home page based on the body class
+ * We can then use that check to perform actions on the home page only
+ *
+ * When the window is resized, we perform this function
+ * $(window).resize(function () {
+ *
+ *    // if we're on the home page, we wait the set amount (in function above) then fire the function
+ *    if( is_home ) { waitForFinalEvent( function() {
+ *
+ *      // if we're above or equal to 768 fire this off
+ *      if( viewport.width >= 768 ) {
+ *        console.log('On home page and window sized to 768 width or more.');
+ *      } else {
+ *        // otherwise, let's do this instead
+ *        console.log('Not on home page, or window sized to less than 768.');
+ *      }
+ *
+ *    }, timeToWaitForLast, "your-function-identifier-string"); }
+ * });
+ *
+ * Pretty cool huh? You can create functions like this to conditionally load
+ * content and other stuff dependent on the viewport.
+ * Remember that mobile devices and javascript aren't the best of friends.
+ * Keep it light and always make sure the larger viewports are doing the heavy lifting.
+ *
+*/
+
+
+/*
  * Put all your regular jQuery in here.
 */
+
 jQuery(document).ready(function($) {
-/**
+	
+	/**
 	 * Initiates Headroom.js
 	 * Header must have an id, the id is referenced below
 	 * CSS can be defined in _headroom.scss 
@@ -109,7 +152,7 @@ jQuery(document).ready(function($) {
 	 *			<div class="mix category-2" data-myorder="8"></div>
 	 *		</div>
 	 */
-	$('#Container').mixItUp();			
+	$('#Container').mixItUp();		
 	/**
 	 * Mobile menu show/hide
 	 */
@@ -130,4 +173,29 @@ jQuery(document).ready(function($) {
 		if ( $screentext.length )
 			$this.attr('title', $screentext.text());
 	});
+
+	/**
+	 * Expand blocks
+	 */
+	$('.expandblock').click(function(e) {
+		e.preventDefault();
+		$(this).toggleClass('active');
+		$(this).next('.expandcontent').slideToggle(200);
+	});
+	/**
+	 * Share links
+	 */
+	$('.share-links').each(function() {
+		// Select the text area on click
+		$(this).on('click', '.share-url', function () {
+			$(this).select();
+		});
+
+		// Open share links in new window
+		$('a', this).click(function(e) {
+			e.preventDefault();
+			window.open($(this).attr('href'), 'Share', 'height=470, width=550, top='+($(window).height()/2 - 225) +', left='+$(window).width()/2 +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+		});
+	});
+
 }); /* end of as page load scripts */
--- assets/js/src/theme.js
+++ assets/js/src/theme.js
@@ -33,8 +33,83 @@ var timeToWaitForLast = 100;
  * Put all your regular jQuery in here.
 */
 jQuery(document).ready(function($) {
+/**
+	 * Initiates Headroom.js
+	 * Header must have an id, the id is referenced below
+	 * CSS can be defined in _headroom.scss 
+	 */
+	var colorbox_params = {
+			rel: 'gal',
+	        maxWidth: '96%',
+	        maxHeight: '90%',
+	        fixed: true
+	    };
+	 // clean up on deployment and keep only specific selectors
+	$('.colorbox').colorbox(colorbox_params);
+	$('.gallery a').colorbox(colorbox_params);
+	$('a[href$=\".jpg\"], a[href$=\".png\"]').colorbox(colorbox_params);
+	
+	/**
+	 * Initiates Headroom.js
+	 * Header must have an id, the id is referenced below
+	 * CSS can be defined in _headroom.scss 
+	 */
+	$("#headroom").headroom({					//define header id here
+		// scroll tolerance in px before state changes
+		"tolerance": 0,
+		// vertical offset in px before element is first unpinned
+		"offset": 147, /* set this to height of your header */
+		"classes": {
+			// when element is initialised
+			"initial": "animated",
+			// when scrolling up
+			"pinned": "slideDown",
+			// when scrolling down
+			"unpinned": "slideUp",
+			// when above offset
+			"top": "headroom--top",
+			// when below offset
+			"notTop": "headroom--not-top"
+		}
+	});
+	
+	/**
+	 * Initiates Flexslider; Flexslider requires a single containing element, <div>, then, a <ul class=”slides”><li><img src='this.jpg'></li></ul> 
+	 * view options at: https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties
+	 *		SAMPLE HTML:
+	 *		<div class="flexslider">
+	 *		  <ul class="slides">
+	 *		    <li>
+	 *		      <img src="slide1.jpg" />
+	 *		    </li>
+	 *		  </ul>
+	 *		</div>
+	 *
+	 *		LOAD CSS 
+	 */ 
+	var flexslider_params = {
+			//slideshowSpeed:4500,
+			//animation:'slide',
+			//direction:'vertical'
+			} 
+	$('.flexslider').flexslider(flexslider_params);
 
-
+	/**
+	 * Initiates MixitUp
+	 *		DIVS INITIATING FILTER
+	 *		<div class="filter" data-filter="all">Show All</div>
+	 *		<div class="filter" data-filter=".category-1">Category 1</div>
+	 *		<div class="filter" data-filter=".category-2">Category 2</div>
+	 *		MAIN DIV
+	 *		 <div id="Container">
+	 *			<div class="mix category-1" data-myorder="2"></div>
+	 *			<div class="mix category-2" data-myorder="4"></div>
+	 *			<div class="mix category-1" data-myorder="1"></div>
+	 *			...
+	 *			<div class="mix category-2" data-myorder="8"></div>
+	 *		</div>
+	 */
+	$('#Container').mixItUp();			
 	/**
 	 * Mobile menu show/hide
 	 */
@@ -55,21 +130,4 @@ jQuery(document).ready(function($) {
 		if ( $screentext.length )
 			$this.attr('title', $screentext.text());
 	});
-
-	/**
-	 * Share links
-	 */
-	$('.share-links').each(function() {
-		// Select the text area on click
-		$(this).on('click', '.share-url', function () {
-			$(this).select();
-		});
-
-		// Open share links in new window
-		$('a', this).click(function(e) {
-			e.preventDefault();
-			window.open($(this).attr('href'), 'Share', 'height=470, width=550, top='+($(window).height()/2 - 225) +', left='+$(window).width()/2 +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
-		});
-	});
-
 }); /* end of as page load scripts */
