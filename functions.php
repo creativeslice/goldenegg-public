f6d8aa4d063c88e349ee6b8bad65ce5d31efa309
Jake Snyder <jacobsnyder@gmail.com>
2014-08-06 12:43:37 -0500

    version number

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ require_once( 'includes/enqueue.php' );
 require_once( 'includes/page-navi.php' );
 #require_once( 'includes/related-posts.php' );
 #require_once( 'includes/youtube-customization.php' );	// Customize iframe and youtube parameters
-#require_once( 'includes/custom-post-types.php' );		// Create custom post types
+require_once( 'includes/custom-post-types.php' );		// Create custom post types
 #require_once( 'includes/nice-search.php' );			// Clean search urls
 #require_once( 'includes/disable-pingback.php' );		// Disable XMLRPC, pingbacks, trackbacks
 #require_once( 'includes/disable-feeds.php' );			// Disable site feeds
@@ -44,3 +44,16 @@ require_once( 'includes/page-navi.php' );
 #require_once( 'includes/lazy-load.php' );				// Lazy load content
 #require_once( 'includes/one-time-code.php' );
 #require_once( 'includes/egg-calendar.php' );			// Flexible calendar (monthly, weekly, daily)
+
+/**
+ * Customize what post types are added to the XML sitemap
+ *
+ * @param   array   $post_types List of post types to be added to the XML Sitemap
+ * @return  array   $post_types Modified list of post types
+ */
+add_filter( 'sewn_seo/post_types', 'custom_sitemap_post_types' );
+function custom_sitemap_post_types( $post_types )
+{
+    $post_types[] = "custom_type";
+    return $post_types;
+}
