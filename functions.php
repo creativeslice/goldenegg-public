c69a09ec1b94652009bb9e89d7520649df825f0e
Tim Bowen <tim@creativeslice.com>
2014-08-23 11:50:33 -0700

    Updated name to 10-up-lazy load so we know where it came from

--- functions.php
+++ functions.php
@@ -41,7 +41,7 @@ require_once( 'includes/page-navi.php' );
 
 // In Development
 
-#require_once( 'includes/lazy-load.php' );				// Lazy load content
+#require_once( 'includes/10up-lazy-load.php' );				// Lazy load content
 #require_once( 'includes/one-time-code.php' );
 #require_once( 'includes/egg-calendar.php' );			// Flexible calendar (monthly, weekly, daily)
 
