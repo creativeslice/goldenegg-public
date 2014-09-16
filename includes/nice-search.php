ca93f83882f822145ce93e7abace2fc0ddc4ab83
CS iMac <contact@creativeslice.com>
2014-09-16 10:33:35 -0700

    Added Relevanssi Search Return Modification

--- includes/nice-search.php
+++ includes/nice-search.php
@@ -22,21 +22,39 @@ function egg_nice_search_redirect()
 	}
 }
 
+/**
+ * Uncomment add_filter('relevanssi_excerpt_content') to modify the Relevanssi Search results
+ * Requires Relevanssi Plugin
+ * Requires in Relevanss Settings:
+ *    (1) Selecting Post Types to Index
+ *    (2) Custom Fields to Index = 'visible'
+ *    (3) Save and Index
+ */
+//add_filter('relevanssi_excerpt_content', 'excerpt_function', 10, 3);
 
-// Order search results by post type
-/*
-add_filter('posts_orderby', 'group_by_post_type', 10, 2);
-function group_by_post_type($orderby, $query) {
-    global $wpdb;
-    if ($query->is_search) {
-        return $wpdb->posts . '.post_type DESC';
+/**
+ * Modifies the Relevanssi results
+ *
+ * @return [string] modified content of the resulting post and its custom fields
+ */
+function excerpt_function($content, $post, $query){
+    $relevanssi_index_post_types = list_searcheable_acf();
+    if(!empty($relevanssi_index_post_types)){
+        $meta_values = '';
+        foreach($relevanssi_index_post_types AS $custom_post_type){ 
+            $meta_value = get_post_meta( $post->ID, $custom_post_type, true);
+            // CreativeSlice Custom: the returned content only includes fields where the search term exists (includes post_content)
+            if(!empty($meta_value) && strpos($meta_value, $query) ){
+                $meta_values .= $meta_value.' ';
+            }
+        }
     }
-    return $orderby;
+    if(!empty($meta_values)){
+        $content .= preg_replace("/\n\r|\r\n|\n|\r/", " ", $meta_values);
+    }
+    return $content;
 }
-*/
 
-
-// custom acf search - searches wp_postmeta for custom fields 
 /**
  * [list_searcheable_acf generates array of all custom fields]
  * @return [array] [list of custom fields]
@@ -51,81 +69,4 @@ function list_searcheable_acf(){
 	}
   return $field_names;
 }
-
-
-/**
- * [advanced_custom_search search that encompasses ACF/advanced custom fields and taxonomies and split expression before request]
- * @param  [query-part/string]      $where    [the initial "where" part of the search query]
- * @param  [object]                 $wp_query []
- * @return [query-part/string]      $where    [the "where" part of the search query as we customized]
- * see https://vzurczak.wordpress.com/2013/06/15/extend-the-default-wordpress-search/
- * credits to Vincent Zurczak for the base query structure/spliting tags section
- */
-function advanced_custom_search( $where, &$wp_query ) {
-
-    global $wpdb;
- 
-    if ( empty( $where ))
-        return $where;
- 
-    // get search expression
-    $terms = $wp_query->query_vars[ 's' ];
-    
-    // explode search expression to get search terms
-    $exploded = explode( ' ', $terms );
-    if( $exploded === FALSE || count( $exploded ) == 0 )
-        $exploded = array( 0 => $terms );
-         
-    // reset search in order to rebuilt it as we whish
-    $where = '';
-    
-    // get searcheable_acf, a list of advanced custom fields you want to search content in
-    $list_searcheable_acf = list_searcheable_acf();
-
-    foreach( $exploded as $tag ) :
-        $where .= " 
-          AND (
-            (wp_posts.post_title LIKE '%$tag%')
-            OR (wp_posts.post_content LIKE '%$tag%')
-            OR EXISTS (
-              SELECT * FROM wp_postmeta
-	              WHERE post_id = wp_posts.ID
-	      
-	                AND (";
-
-        foreach ($list_searcheable_acf as $searcheable_acf) :
-          if ($searcheable_acf == $list_searcheable_acf[0]):
-            $where .= " (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
-          else :
-            $where .= " OR (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
-          endif;
-        endforeach;
-
-	        $where .= ")
-            )
-            OR EXISTS (
-              SELECT * FROM wp_comments
-              WHERE comment_post_ID = wp_posts.ID
-                AND comment_content LIKE '%$tag%'
-            )
-            OR EXISTS (
-              SELECT * FROM wp_terms
-              INNER JOIN wp_term_taxonomy
-                ON wp_term_taxonomy.term_id = wp_terms.term_id
-              INNER JOIN wp_term_relationships
-                ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
-              WHERE (
-          		taxonomy = 'post_tag'
-            		OR taxonomy = 'category'          		
-            		OR taxonomy = 'myCustomTax'
-          		)
-              	AND object_id = wp_posts.ID
-              	AND wp_terms.name LIKE '%$tag%'
-            )
-        )";
-    endforeach;
-    return $where;
-}
- 
-add_filter( 'posts_search', 'advanced_custom_search', 500, 2 );
 ?>
