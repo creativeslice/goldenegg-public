d3a9a59958e982194d36529edebf9cbff967c334
Tim Bowen <tim@creativeslice.com>
2015-02-20 08:32:39 -0700

    function ordering

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * Set up the theme
  */
 define( 'EGG_DEVELOPER',     'Creative Slice' );
-define( 'EGG_DEVELOPER_URL', 'http://creativeslice.com/' );
+define( 'EGG_DEVELOPER_URL', 'https://creativeslice.com/' );
 
 #$egg_info = wp_get_theme();
 #define( 'EGG_VERSION',        $egg_info->Version );
@@ -23,19 +23,20 @@ require_once( 'admin/tinymce.php' );
 #require_once( 'admin/recently-updated-content.php' ); 	// Shows recently updated content. REQUIRES customization
 #require_once( 'admin/disable-comments.php' );          // Completely remove comments from the admin area
 
-// Front end
-require_once( 'includes/cleanup.php' );
-require_once( 'includes/theme-support.php' );
+// Front End
 require_once( 'includes/enqueue.php' );
+require_once( 'includes/theme-support.php' );
 require_once( 'includes/page-navi.php' );
-#require_once( 'includes/weather-functions.php' );
-#require_once( 'includes/related-content.php' );		// Associated content and related posts
-#require_once( 'includes/youtube-customization.php' );	// Customize iframe and youtube parameters
-#equire_once( 'includes/custom-post-types.php' );		// Create custom post types
-#require_once( 'includes/nice-search.php' );			// Clean search urls AND Relevanssi custom fields.
+require_once( 'includes/cleanup.php' );					// Cleanup WordPress scripts
+#require_once( 'includes/cleanup-plugins.php' );		// Cleanup plugins like Gravity Forms
+#require_once( 'includes/custom-post-types.php' );		// Create custom post types
 #require_once( 'includes/disable-pingback.php' );		// Disable XMLRPC, pingbacks, trackbacks
 #require_once( 'includes/disable-feeds.php' );			// Disable site feeds
-#require_once( 'includes/cleanup-plugins.php' );		// Cleanup commonly used plugins
+
+#require_once( 'includes/related-content.php' );		// Associated content and related posts
+#require_once( 'includes/nice-search.php' );			// Clean search urls & Relevanssi custom fields
+#require_once( 'includes/youtube-customization.php' );	// Customize iframe and youtube parameters
+#require_once( 'includes/weather-functions.php' );		// Pulling in weather and caching it
 #require_once( 'includes/10up-lazy-load.php' );			// Lazy load images based on 10up plugin
 
 // In Development
@@ -44,7 +45,6 @@ require_once( 'includes/page-navi.php' );
 
 /**
  * Adds ACF Options functionality
- *
  */
 /*
 if(function_exists('acf_add_options_page')) { 
@@ -61,14 +61,20 @@ if(function_exists('acf_add_options_page')) {
 
 /**
  * Requires https://wordpress.org/plugins/sewn-in-xml-sitemap/
- * Customize which post types are added to the XML sitemap
+ * Works with https://wordpress.org/plugins/sewn-in-simple-seo/
+ * Completely replace the post types in the XML sitemap
+ *
+ * This will replace the default completely. Returns: array('page','post','event')
+ *
+ * The result is to remove 'post' and 'page' post types and to add 'news' and 
+ * 'event' post types
  *
  * @param   array   $post_types List of post types to be added to the XML Sitemap
  * @return  array   $post_types Modified list of post types
  */
-/*
-add_filter( 'sewn_seo/post_types', 'custom_sitemap_post_types' );
-function custom_sitemap_post_types( $post_types )
+ /*
+add_filter( 'sewn/seo/post_types', 'custom_seo_post_types' );
+function custom_seo_post_types( $post_types )
 {
     $post_types = array('page','post');
     return $post_types;
