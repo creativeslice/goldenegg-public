5bd50f7978b9b63f88850550d8212c5a13c9fcb5
Tim Bowen <tim@creativeslice.com>
2018-05-17 19:36:09 -0700

    fixing favicon typo

--- components/favicons/favicons.php
+++ components/favicons/favicons.php
@@ -1,12 +1,13 @@
+<!-- https://www.favicon-generator.org/ -->
 
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/components/Favicons/img/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/components/Favicons/img/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri(); ?>/components/Favicons/img/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/components/Favicons/img/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/components/Favicons/img/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/components/Favicons/img/favicon-16x16.png">
-<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/components/Favicons/img/manifest.json">
-<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/components/Favicons/img/ms-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/components/favicons/img/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/components/favicons/img/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri(); ?>/components/favicons/img/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/components/favicons/img/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/components/favicons/img/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/components/favicons/img/favicon-16x16.png">
+<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/components/favicons/img/manifest.json">
+<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/components/favicons/img/ms-icon-144x144.png">
 <meta name="msapplication-TileColor" content="#e8c200">
 <meta name="theme-color" content="#e8c200">
 
