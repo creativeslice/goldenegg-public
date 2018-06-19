a9128b44b10d857de9987cbf0782258544460197
Tim Bowen <tim@creativeslice.com>
2018-06-18 21:21:05 -0700

    sample lazy image

--- front-page.php
+++ front-page.php
@@ -10,6 +10,17 @@
 
 		<section class="entryContent">
 			<?php the_post(); the_content(); ?>
+			
+			<figure class="lazy70">
+				<img
+					data-sizes="100vw"
+					data-src="<?php echo get_template_directory_uri(); ?>/assets/img/img_600x400.jpg"
+					data-srcset="<?php echo get_template_directory_uri(); ?>/assets/img/img_1000x700.jpg 1000w,
+					<?php echo get_template_directory_uri(); ?>/assets/img/img_600x400.jpg 600w"
+					alt="Gold Egg Alt Text"
+					class="lazyload lazypreload" />
+			</figure>
+			
 		</section>
 
 	</article>
