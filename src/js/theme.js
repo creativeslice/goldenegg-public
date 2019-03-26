2b7a4513e71bde52243529fc5115f3ab1d5c701f
Tim Bowen <tim@creativeslice.com>
2019-03-26 10:26:20 -0700

    better widow script spacing

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,5 +1,5 @@
 /*
- * Put all your regular jQuery in here.
+ * Wrap jQuery JS in ready class
 */
 jQuery(document).ready(function($) {
 	
@@ -15,33 +15,9 @@ jQuery(document).ready(function($) {
 	 */
 	$('.articleHeader h1').each(function () {
 		var string = $(this).html();
-		string = string.replace(/ ([^ ]*)$/, '&nbsp;$1');
+		string = string.replace(/ ([^ ]*)$/, '&#8239;$1');
 		$(this).html(string);
 	});
-
-
-	/**
-	 * Detect scroll position and add sticky class to body
-	 */
-	 
-	// get initial offset height of #stickyTrigger
-	if ( $('#logo').length ){
-		elementOffset = $('#logo').offset().top;
-	}
-
-	// NEED TO UPDATE timeToWaitForLast based on Jake's function
-	window.onscroll = function() {
-		var windowTop = $(window).scrollTop();
-
-		// checks for #stickyTrigger ID and will trigger "sticky"
-		if ( $('#logo').length ) {
-			var distance = (elementOffset - windowTop + 122); // fixed header offset
-			if (distance < 0 ) {
-				$('body').addClass('sticky');
-			} else {
-				$('body').removeClass('sticky');
-			}
-		}
-	}
+	
 
 });
