37317d9f6b06351241d81345bdbc0115fdbbb12e
Jeremy Englert <je@materiell.com>
2021-02-21 20:50:23 -0700

    Setup livereload

--- wp-content/themes/goldenegg/includes/enqueue.php
+++ wp-content/themes/goldenegg/includes/enqueue.php
@@ -7,9 +7,9 @@ add_action( 'wp_enqueue_scripts', 'egg_styles', 999 );
 function egg_styles() {
 	
 	/* Add modified date for cache busting */
-	$csschanged = filemtime( realpath(__DIR__ . '/..') . '/assets/css/style.css' );
+	$csschanged = filemtime( realpath(__DIR__ . '/..') . '/assets/css/styles.css' );
 	
-	wp_register_style( 'egg-stylesheet', get_stylesheet_directory_uri() . '/assets/css/style.css?v=' . $csschanged, array(), '', 'all' );
+	wp_register_style( 'egg-stylesheet', get_stylesheet_directory_uri() . '/assets/css/styles.css?v=' . $csschanged, array(), '', 'all' );
 	wp_enqueue_style( 'egg-stylesheet');
 }
 
@@ -30,6 +30,12 @@ function egg_scripts() {
 	/* Scripts */
 	wp_register_script( 'egg-js', get_stylesheet_directory_uri() . '/assets/js/scripts.js?' . $jschanged, array( 'jquery' ), '', false );
 	wp_enqueue_script( 'egg-js');
+
+    /* Enqueue Live-Reload only in local environent */
+    if ( $_SERVER["SERVER_ADDR"] == '172.17.0.2' || $_SERVER["SERVER_ADDR"] == '127.0.0.1') {
+        /* Scripts */
+        wp_enqueue_script('live-reload', 'http://localhost:35729/livereload.js' , '', '', false );
+    }
 }
 
 
@@ -64,3 +70,4 @@ function egg_dequeue_jquery_migrate( &$scripts ) {
 		$scripts->add( 'jquery', false, array( 'jquery-core' ), '1.10.2' );
 	}
 }
+
