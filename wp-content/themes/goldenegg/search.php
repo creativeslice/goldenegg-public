dbd027cc176e600acfd8a72ba5d56f1d429d96d8
Sam DeVore <sdevore@gmail.com>
2021-09-22 16:27:21 -0700

    working on linting stuff for vs code

--- wp-content/themes/goldenegg/search.php
+++ wp-content/themes/goldenegg/search.php
@@ -1,41 +1,53 @@
-<?php get_template_part('partials/header/header'); // Search Results ?>
-
+<?php get_template_part( 'partials/header/header' ); // Search Results ?>
 <div id="content" class="wrap">
-	
+
 	<h1 class="archiveTitle"><span>Search Results for:</span> <?php echo get_search_query(); ?></h1>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php
+	if ( have_posts() ) :
+		while ( have_posts() ) :
+			the_post();
+			?>
 
-		<?php // PAGE
-		if( 'page' == get_post_type() ):?>
-		<article <?php post_class('cf'); ?>>
-			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-			<?php the_excerpt(); ?>
+			<?php
+				// PAGE
+			if ( 'page' == get_post_type() ) :
+				?>
+		<article <?php post_class( 'cf' ); ?>>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<?php the_excerpt(); ?>
 		</article>
-		
-		<?php // POST
-		elseif( 'post' == get_post_type() ): ?>
-		<article <?php post_class('cf'); ?>>
-			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-			<span class="byline">Posted <?php the_time('F j, Y'); ?></span>
-			<?php the_excerpt(); ?>
+
+				<?php
+				// POST!
+				elseif ( 'post' == get_post_type() ) :
+					?>
+		<article <?php post_class( 'cf' ); ?>>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<span class="byline">Posted <?php the_time( 'F j, Y' ); ?></span>
+					<?php the_excerpt(); ?>
 		</article>
-	
-		<?php // CPT
-		else : ?>
-		<article <?php post_class('cf'); ?>>
-			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-			<?php the_excerpt(); ?>
+
+					<?php
+					// CPT!
+				else :
+					?>
+		<article <?php post_class( 'cf' ); ?>>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<?php the_excerpt(); ?>
 		</article>
-		
+
 		<?php endif; ?>
 
-	<?php endwhile;
+			<?php
+	endwhile;
 		get_template_part( 'partials/archive-nav/archive-nav' );
-	else : 
+	else :
 		echo '<h1>Nothing Found Here</h1>';
-	endif; ?>
+	endif;
+	?>
 
 </div>
 
-<?php get_template_part('partials/footer/footer');
+<?php
+get_template_part( 'partials/footer/footer' );
--- wp-content/themes/goldenegg/single.php
+++ wp-content/themes/goldenegg/single.php
@@ -1,27 +1,32 @@
-<?php get_template_part('partials/header/header'); // Template Name: Has Sidebar ?>
+<?php get_template_part( 'partials/header/header' ); // Template Name: Has Sidebar ?>
 
 <article id="content" class="wrapperWide">
-	
+
 	<main class="postContentBlocks gutenbergBlocks">
-		<?php the_post(); the_content(); ?>
-    </main>
-    
-    <?php get_sidebar(); ?>
-    
-    <footer class="articleFooter">
+		<?php
+		the_post();
+		the_content();
+		?>
+	</main>
+
+	<?php get_sidebar(); ?>
+
+	<footer class="articleFooter">
 
 		<?php the_tags( '<p class="tags"><span class="tags-title">Tags:</span> ', ', ', '</p>' ); ?>
-		
-		<?php // Social Share Links
-			get_template_part( 'partials/share-links/share-links' ); 
+
+		<?php
+		// Social Share Links.
+			get_template_part( 'partials/share-links/share-links' );
 		?>
 
-		<span class="postPrev"><?php previous_post_link('%link', '&lsaquo;&nbsp;Previous'); ?></span>
-		<span class="postNext"><?php next_post_link('%link', 'Next&nbsp;&rsaquo;'); ?></span>
+		<span class="postPrev"><?php previous_post_link( '%link', '&lsaquo;&nbsp;Previous' ); ?></span>
+		<span class="postNext"><?php next_post_link( '%link', 'Next&nbsp;&rsaquo;' ); ?></span>
 
 	</footer>
 
 </article>
 
-<?php get_template_part('partials/footer/footer');
-	
+<?php
+get_template_part( 'partials/footer/footer' );
+
